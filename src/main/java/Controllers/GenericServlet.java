cb7d1d1bd3fded70339a570c55ceb76170ff6f12
Maxwell Gyamfi <46795132+Maxwell2Gyamfi@users.noreply.github.com>
2020-11-24 16:53:40 +0000

    pomcfile updated
    added updated pom.xml file

--- src/main/java/Controllers/GenericServlet.java
+++ src/main/java/Controllers/GenericServlet.java
@@ -12,13 +12,17 @@ import java.sql.Connection;
 
 public class GenericServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
     }
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         Connection con = Database.getInstance().getConnection();
+        System.out.println("I am hereree");
+        System.out.println(con);
+        
         request.setAttribute("HelloWorld", "Hello World!");
         request.setAttribute("DatabaseString", con.toString());
         request.getRequestDispatcher("index.jsp").forward(request, response);
+        
     }
 }
