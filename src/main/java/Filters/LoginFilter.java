341efa73321e22fc59b1294eae2a059e23417601
Tom Goring <mail@tomgoring.co.uk>
2020-12-12 22:47:16 +0000

    Fix abstract filter methods & downgrade web.xml version so old versions of Netbeans function correctly

--- src/main/java/Filters/LoginFilter.java
+++ src/main/java/Filters/LoginFilter.java
@@ -9,6 +9,12 @@ import java.io.IOException;
 
 @WebFilter(filterName = "LoginFilter")
 public class LoginFilter implements Filter {
+    @Override
+    public void destroy() {}
+    @Override
+    public void init(FilterConfig config) throws ServletException {}
+
+    @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) resp;
