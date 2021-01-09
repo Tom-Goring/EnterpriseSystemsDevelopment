737620a0263a318bf639ecee3fc92ae623a7bdfb
Charlie Williams <charliewilliamsweb@gmail.com>
2021-01-09 20:49:06 +0000

    Create appointment unit tests
    * Refactor model unit test directory

---
+++ src/test/ControllersTest/AppointmentServletTest.java
@@ -0,0 +1,119 @@
+package ControllersTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AppointmentServletTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void doGet() {
+    }
+
+    @Test
+    void Given_AddGetAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_DeleteGetAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_ViewGetAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_UpdateGetAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_ConfirmPostAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_AvailabilityPostAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_ViewSelectedPostAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_ViewAllAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_DeletePostAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_UpdatePostAction_Then_SelectDispatcherCase() {
+        // Given
+
+        // When
+
+        // Then
+    }
+
+    @Test
+    void Given_Date_Then_ReturnDayOfWeek() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
---
+++ src/test/ModelsTest/AppointmentTest/AppointmentDAOTest.java
@@ -0,0 +1,25 @@
+package ModelsTest.AppointmentTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AppointmentDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void Given_ValidAppointment_Then_InsertAppointment() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
