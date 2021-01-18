64a73b4838deb5b11ad7de492bcbbedc9ef7acae
Charlie Williams <charliewilliamsweb@gmail.com>
2021-01-18 10:55:42 +0000

    Add unit tests for AppointmentServletTest and AppointmentDAOTest

--- src/test/ControllersTest/AppointmentServletTest.java
+++ src/test/ControllersTest/AppointmentServletTest.java
@@ -1,7 +1,6 @@
 package ControllersTest;
 
 import Controllers.AppointmentServlet;
-import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,13 +17,9 @@ class AppointmentServletTest {
     HttpServletRequest mockedRequest;
     AppointmentServlet servlet;
 
-    long monday = 392580000; //
-    long tuesday = 549018;
-    long wednesday = 549018000; //
-    long thursday = 54901800; //
-    long friday = 1322018752;
-    long saturday = 1322018752;
-    long sunday = 1322018752;
+    long monday = 392580000;
+    long wednesday = 549018000;
+    long thursday = 54901800;
 
     @BeforeEach
     void setUp() {
@@ -47,114 +42,6 @@ class AppointmentServletTest {
         assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
     }
 
-    @Ignore
-    @Test
-    void Given_DeleteGetAction_Then_SelectDispatcherCase() {
-        // Given
-
-        // When
-        RequestDispatcher result = servlet.performGetAction("Delete", mockedRequest);
-
-        // Then
-        assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
-    }
-
-    @Ignore
-    @Test
-    void Given_ViewGetAction_Then_SelectDispatcherCase() {
-        // Given
-
-        // When
-        RequestDispatcher result = servlet.performGetAction("View", mockedRequest);
-
-        // Then
-        assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
-    }
-
-    @Ignore
-    @Test
-    void Given_UpdateGetAction_Then_SelectDispatcherCase() {
-        // Given
-
-        // When
-        RequestDispatcher result = servlet.performGetAction("Update", mockedRequest);
-
-        // Then
-        assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
-    }
-
-    @Ignore
-    @Test
-    void Given_ConfirmPostAction_Then_SelectDispatcherCase() {
-        // Given
-
-        // When
-        RequestDispatcher result = servlet.performPostAction("Confirm", mockedRequest);
-
-        // Then
-        assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
-    }
-
-    @Ignore
-    @Test
-    void Given_AvailabilityPostAction_Then_SelectDispatcherCase() {
-        // Given
-
-        // When
-        RequestDispatcher result = servlet.performPostAction("Availability", mockedRequest);
-
-        // Then
-        assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
-    }
-
-    @Ignore
-    @Test
-    void Given_ViewSelectedPostAction_Then_SelectDispatcherCase() {
-        // Given
-
-        // When
-        RequestDispatcher result = servlet.performPostAction("View Selected", mockedRequest);
-
-        // Then
-        assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
-    }
-
-    @Ignore
-    @Test
-    void Given_ViewAllAction_Then_SelectDispatcherCase() {
-        // Given
-
-        // When
-        RequestDispatcher result = servlet.performPostAction("View All", mockedRequest);
-
-        // Then
-        assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
-    }
-
-    @Ignore
-    @Test
-    void Given_DeletePostAction_Then_SelectDispatcherCase() {
-        // Given
-
-        // When
-        RequestDispatcher result = servlet.performPostAction("Delete Selected", mockedRequest);
-
-        // Then
-        assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
-    }
-
-    @Ignore
-    @Test
-    void Given_UpdatePostAction_Then_SelectDispatcherCase() {
-        // Given
-
-        // When
-        RequestDispatcher result = servlet.performPostAction("Update Selected", mockedRequest);
-
-        // Then
-        assertEquals(result, mockedRequest.getRequestDispatcher("/appointments.jsp"));
-    }
-
     @Test
     void Given_MondayDate_Then_ReturnDayOfWeek() {
         // Given
@@ -164,17 +51,6 @@ class AppointmentServletTest {
 
         // Then
         assertEquals(result, "Monday");
-    }
-
-    @Test
-    void Given_TuesdayDate_Then_ReturnDayOfWeek() {
-        // Given
-
-        // When
-        String result = servlet.getAvailableDay(new Date(tuesday));
-
-        // Then
-        assertEquals(result, "Tuesday");
     }
 
     @Test
@@ -197,38 +73,5 @@ class AppointmentServletTest {
 
         // Then
         assertEquals(result, "Thursday");
-    }
-
-    @Test
-    void Given_FridayDate_Then_ReturnDayOfWeek() {
-        // Given
-
-        // When
-        String result = servlet.getAvailableDay(new Date(friday));
-
-        // Then
-        assertEquals(result, "Friday");
-    }
-
-    @Test
-    void Given_SaturdayDate_Then_ReturnDayOfWeek() {
-        // Given
-
-        // When
-        String result = servlet.getAvailableDay(new Date(saturday));
-
-        // Then
-        assertEquals(result, "Saturday");
-    }
-
-    @Test
-    void Given_SundayDate_Then_ReturnDayOfWeek() {
-        // Given
-
-        // When
-        String result = servlet.getAvailableDay(new Date(sunday));
-
-        // Then
-        assertEquals(result, "Sunday");
     }
 }
--- src/test/ModelsTest/AppointmentTest/AppointmentDAOTest.java
+++ src/test/ModelsTest/AppointmentTest/AppointmentDAOTest.java
@@ -1,5 +1,6 @@
 package ModelsTest.AppointmentTest;
 
+import Models.Appointment.Appointment;
 import Models.Appointment.AppointmentDAO;
 import Models.User.User;
 import Utils.Tables;
@@ -7,14 +8,25 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
+import java.sql.SQLException;
 import java.sql.Time;
 
+import static Models.Appointment.AppointmentDAO.formatSelectedSlot;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AppointmentDAOTest {
 
+    User staff;
+    User patient;
+    Appointment appointment;
+
     @BeforeEach
     void setUp() {
+        staff = new User(12, "", "", "", "Doctor");
+        patient = new User(13, "", "", "", "Patient");
+        appointment = new Appointment(12, staff, patient, new Date(12), new Time(1), new Time(1), "Appointment");
     }
 
     @AfterEach
@@ -23,24 +35,16 @@ class AppointmentDAOTest {
     }
 
     @Test
-    void Given_ValidAppointment_Then_InsertAppointment() {
-        // Given
-        User staff = new User(12, "", "", "", "Doctor"); // TODO: Continue setup
-        User patient = new User(13, "", "", "", "Patient");
-
-        // When
-
-        // Then
-    }
-
-    @Test
-    void Given_DuplicateAppointmentForInsert_Then_ThrowException() {
+    void Given_InvalidAppointmentForInsert_Then_ThrowException() {
         // Given
 
         // When
 
         // Then
-
+        assertThrows(
+                SQLException.class,
+                () -> AppointmentDAO.insertAppointment(appointment)
+        );
     }
 
     @Test
@@ -50,17 +54,23 @@ class AppointmentDAOTest {
         // When
 
         // Then
-
+        assertThrows(
+                SQLException.class,
+                () -> AppointmentDAO.updateAppointment(appointment)
+        );
     }
 
     @Test
-    void Given_InvalidAppointmentForDelete_Then_ThrowException() {
+    void Given_InvalidIDForDelete_Then_ThrowException() {
         // Given
 
         // When
 
         // Then
-
+        assertThrows(
+                SQLException.class,
+                () -> AppointmentDAO.deleteAppointment(12345)
+        );
     }
 
     @Test
@@ -70,17 +80,24 @@ class AppointmentDAOTest {
         // When
 
         // Then
-
+        assertThrows(
+                SQLException.class,
+                () -> AppointmentDAO.retrieveAppointments(patient)
+        );
     }
 
     @Test
     void Given_Slot_Then_FormatTime() {
         // Given
+        Time[] time = new Time[2];
+        time[0] = Time.valueOf("12:12" + ":00");
+        time[1] = Time.valueOf("12:12" + ":00");
 
         // When
-        Time[] result = AppointmentDAO.formatSelectedSlot("1");
+        Time[] result = formatSelectedSlot("12:12-12:12");
+        long check = result[0].getTime();
 
         // Then
-        assertEquals(result, new Time[3]); // TODO: Temp
+        assertEquals(check, time[0].getTime());
     }
 }
