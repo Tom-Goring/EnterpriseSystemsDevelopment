aa279ac8a6bb0b833fe8aab36ad0d84be206634b
Charlie Williams <charliewilliamsweb@gmail.com>
2021-01-19 09:59:46 +0000

    Create test files

---
+++ src/test/ControllersTest/IssuePrescriptionServletTest.java
@@ -0,0 +1,20 @@
+package ControllersTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IssuePrescriptionServletTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getServletInfo() {
+    }
+}
---
+++ src/test/ModelsTest/PrescriptionTest/PrescriptionDAOTest.java
@@ -0,0 +1,24 @@
+package ModelsTest.PrescriptionTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PrescriptionDAOTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void insertPrescription() {
+    }
+
+    @Test
+    void getAllPrescriptionsForUser() {
+    }
+}
